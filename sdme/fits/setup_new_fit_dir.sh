317b4d32fcf1786a95ca9e3c987a0e3b9d9c3d17
Gabriel A Rodriguez Linera <gabyrod7@gmail.com>
2023-09-11 15:29:16 -0400

    update setup

--- sdme/fits/setup_new_fit_dir.sh
+++ sdme/fits/setup_new_fit_dir.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+if [ -z "$2" ]
 then
-	echo "Missing directory name. Run as:"
-	echo "./setup_new_fit_dir.sh <direcotry_name>"
+	echo "Missing directory name or run period. Run as:"
+	echo "./setup_new_fit_dir.sh <direcotry_name> <run_period>"
 else
 	mkdir $1
 	cp ../sdme_scripts/* $1
 	cd $1
-	sed -i s/sdme_dir/$1/ cmd.sh
+	sed -i s/sdme_dir/$1 cmd.sh
+	sed -i s/DATA/$2/g divideData.pl
+	sed -i s/DATA/$2/g fitsdme.py
 fi
 
