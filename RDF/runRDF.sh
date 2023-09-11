40a642609c9677437f422f1da3c1febc40710c8d
Gabriel A Rodriguez Linera <gabyrod7@gmail.com>
2023-09-11 15:38:10 -0400

    miscellenia

--- RDF/runRDF.sh
+++ RDF/runRDF.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 ### Produce histograms to be analyzed with RDataFrames
-root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_dat_sp17.root", "hist_dat_sp17.root", 0)'
-root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_acc_sp17.root", "hist_acc_sp17.root", 0)'
-# root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_dat_gluex1.root", "hist_dat.root", 0)'
-# root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_acc_gluex1.root", "hist_acc.root", 0)'
-# root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_acc_sdme1.root", "hist_acc_sdme.root", 0)'
+# root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_dat_sp17.root", "hist_dat_sp17.root", 0)'
+# root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_acc_sp17.root", "hist_acc_sp17.root", 0)'
 
+# root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_dat_fa18.root", "hist_dat_fa18.root", 0)'
+# root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_acc_fa18.root", "hist_acc_fa18.root", 0)'
+
+root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_dat_gluex1.root", "hist_dat_gluex1.root", 0)'
+root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_acc_gluex1.root", "hist_acc_gluex1.root", 0)'
+
+# root -l -b -q 'RDF_ana.C(20, "../DSelector/ftrees/ftree_acc_sdme1.root", "hist_acc_sdme.root", 0)'
