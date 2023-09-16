8cdf5b191867cb48e1c98dd2fd4d099cfa4356c5
Gabriel A Rodriguez Linera <gabyrod7@gmail.com>
2023-09-16 15:21:20 -0400

    check if directory name ends with / (this no good)

--- sdme/fits/setup_new_fit_dir.sh
+++ sdme/fits/setup_new_fit_dir.sh
@@ -4,6 +4,10 @@ if [ -z "$2" ]
 then
 	echo "Missing directory name or run period. Run as:"
 	echo "./setup_new_fit_dir.sh <direcotry_name> <run_period>"
+# check if directory name end with /
+elif [[ $1 == */ ]]
+then
+	echo "Directory name should not end with /"
 else
 	mkdir $1
 	cp ../sdme_scripts/* $1
