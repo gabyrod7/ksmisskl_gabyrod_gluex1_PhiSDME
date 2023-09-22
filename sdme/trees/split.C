ef497f0ede2666ee5ce1ec962b66a6e301869838
Gabriel A Rodriguez Linera <gabyrod7@gmail.com>
2023-09-22 10:20:22 -0400

    make it easier to change a single cut

--- sdme/trees/split.C
+++ sdme/trees/split.C
@@ -7,34 +7,50 @@
 #include <string>
 #include <iostream>
 
-void Split(std::string inf_name, std::string data, std::string run, int n_threads = 4, std::string ext = "", std::string filter = "", bool verbose = false);
+void Split(std::string inf_name, std::string data, std::string dir, int n_threads = 4, std::string ext = "", std::string filter = "", bool verbose = false);
 void runSplit(std::vector<std::string> inf_names, std::string dir, int n_threads = 4, std::string ext = "", std::string cuts = "", std::string gen_cuts = "", bool verbose = false);
+std::string set_cuts(std::map<std::string, std::string> cuts_list, std::pair<std::string, std::string> change_cut = {"", ""});
 
 void split() {
 	std::vector<std::string> inf_names;
-	std::string dir;
+	std::string dir = "";
 	int n_threads = 20;
 	std::string ext = "";
-	std::string cuts;
+	std::string cuts = "";
 	std::string gen_cuts = "";
 
-//	// Spring 2017 data for SDME
-	inf_names = {"ftree_dat_sp17.root", "ftree_acc_sp17.root", "ftree_gen_sp17.root"};	
-	dir = "sp17";
-	cuts = "missing_mass > 0.3 && missing_mass < 0.7 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.5 && mkskl > 1.005 && mkskl < 1.05";
+	std::map<std::string, std::string> cuts_list = {{"mkskl", "mkskl > 1.005 && mkskl < 1.04"},
+													{"mmiss", "missing_mass > 0.3 && missing_mass < 0.7"},
+													{"mandel_t", "mandel_t < 1.5"},
+													{"fs", "flight_significance > 4"},
+													{"chisq", "chisq_ndf < 4"},
+													{"ntracks", "num_unused_tracks == 0"},
+													{"nshowers", "num_unused_showers < 3"},
+													{"proton_z_vertex", "proton_z_vertex > 52 && proton_z_vertex < 78"},
+													{"beam_energy", "beam_energy > 8.2 && beam_energy < 8.8"}};
+
+	// Set nominal cuts
+	cuts = set_cuts(cuts_list);
 	gen_cuts = "mandel_t > 0.15 && mandel_t < 1.5";
+
+	// Spring 2017 data for SDME
+	inf_names = {"ftree_dat_sp17.root", "ftree_acc_sp17.root", "ftree_gen_sp17.root"};
+	dir = "sp17";
 	runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
-//	// Spring 2018 data for SDME
-//	inf_names = {"ftree_dat_sp18.root", "ftree_acc_sp18.root", "ftree_gen_sp18.root"};	
-//	dir = "sp18";
-//	runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
+	// Spring 2018 data for SDME
+	inf_names = {"ftree_dat_sp18.root", "ftree_acc_sp18.root", "ftree_gen_sp18.root"};	
+	dir = "sp18";
+	//runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
 	// Fall 2018 data for SDME
 	inf_names = {"ftree_dat_fa18.root", "ftree_acc_fa18.root", "ftree_gen_fa18.root"};	
 	dir = "fa18";
 	// runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
-//	// GlueX-I data for SDME
-//	inf_names = {"ftree_dat_gluex1.root", "ftree_acc_gluex1.root", "ftree_gen_gluex1.root"};	
-//	dir = "gluex1";
+	dir = "fa18_weighted";
+	ext = "fa18";
+	// runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
+	// GlueX-I data for SDME
+	inf_names = {"ftree_dat_gluex1.root", "ftree_acc_gluex1.root", "ftree_gen_gluex1.root"};	
+	dir = "gluex1";
 //	runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
 
 //	// GlueX-1 data for SDME measurement. Beam energy 8.2-8.4
@@ -84,26 +100,26 @@ void split() {
 
 	// GlueX-1 data for SDME measurement. mkskl 1.010-1.030
 	inf_names = {"ftree_dat_gluex1.root", "ftree_acc_gluex1.root", "ftree_gen_gluex1.root"};	
-//	dir = "mkskl1";
-//	cuts = "&& mkskl > 1.010 && mkskl < 1.030 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
-//	gen_cuts = "";
-//	runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
-//	// mkskl 1.005-1.04
-//	dir = "mkskl2";
-//	cuts = "&& mkskl > 1.005 && mkskl < 1.040 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
-//	runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
-//	// mkskl 1-1.06
-//	dir = "mkskl3";
-//	cuts = "&& mkskl > 1.000 && mkskl < 1.060 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
-//	runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
-//	// mkskl 1.005-1.020
-//	dir = "mkskl_low";
-//	cuts = "&& mkskl > 1.000 && mkskl < 1.020 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
-//	runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
-//	// mkskl 1.020-1.050
-//	dir = "mkskl_high";
-//	cuts = "&& mkskl > 1.020 && mkskl < 1.050 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
-//	runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
+	dir = "mkskl1";
+	cuts = "mkskl > 1.010 && mkskl < 1.030 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
+	// gen_cuts = "";
+	// runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
+	// mkskl 1.005-1.04
+	dir = "mkskl2";
+	cuts = "mkskl > 1.005 && mkskl < 1.040 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
+	// runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
+	// mkskl 1-1.06
+	dir = "mkskl3";
+	cuts = "mkskl > 1.000 && mkskl < 1.060 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
+	// runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
+	// // mkskl 1.005-1.020
+	// dir = "mkskl_low";
+	// cuts = "&& mkskl > 1.000 && mkskl < 1.020 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
+	// runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
+	// // mkskl 1.020-1.050
+	// dir = "mkskl_high";
+	// cuts = "&& mkskl > 1.020 && mkskl < 1.050 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
+	// runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
 
 //	// Spring 2017 data for SDME
 //	inf_names = {"../../DSelector/ftree_acc_sdme1.root", "ftree_acc_sp17.root", "ftree_gen_sp17.root"};	
@@ -125,12 +141,28 @@ void split() {
 //	cuts = "&& mkskl > 1.020 && mkskl < 1.050 && missing_mass > 0.30 && missing_mass < 0.70 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.50";
 //	runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
 
-
 //	// GlueX-I no pipi-sideband 
 //	inf_names = {"ftree_dat_gluex1.root", "ftree_acc_gluex1.root", "ftree_gen_gluex1.root"};	
 //	dir = "no_sideband";
 //	cuts = "&& missing_mass > 0.3 && missing_mass < 0.7 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t < 1.5 && mkskl > 1.005 && mkskl < 1.05 && mpipi > 0.48 && mpipi < 0.52";
 //	runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
+
+	// For Partial Wave Analysis
+	inf_names = {"ftree_dat_gluex1.root", "ftree_acc_gluex1.root", "ftree_gen_gluex1.root"};	
+	dir = "pwa_t_0.15_0.25_v2";
+	ext = "gluex1";
+	cuts = "missing_mass > 0.3 && missing_mass < 0.7 && flight_significance > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t > 0.15 && mandel_t < 0.25 && mkskl > 1.00 && mkskl < 1.10";
+	gen_cuts = "mandel_t > 0.15 && mandel_t < 0.25";
+	// runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
+
+	// Spring 2017 data for SDME
+	inf_names = {"ftree_dat_fa18.root", 
+			"/d/grid15/gabyrod7/analysis/ksmisskl_gabyrod_gluex1_PhiSDME/DSelector/ftrees/ftree_acc_fa18_shifted_v2.root", 
+			"/d/grid15/gabyrod7/analysis/ksmisskl_gabyrod_gluex1_PhiSDME/DSelector/ftrees/ftree_gen_fa18_shifted_v2.root"};
+	dir = "fa18_shifted_test";
+	cuts = set_cuts(cuts_list);
+	ext = "fa18";
+	// runSplit(inf_names, dir, n_threads, ext, cuts, gen_cuts);
 }
 
 void runSplit(std::vector<std::string> inf_names, std::string dir, int n_threads = 4, std::string ext = "", std::string cuts = "", std::string gen_cuts = "", bool verbose = false) {
@@ -139,16 +171,18 @@ void runSplit(std::vector<std::string> inf_names, std::string dir, int n_threads
 		gSystem->Exec( ("mkdir "+dir+"/").c_str() );
 	}
 
+	if((dir == "sp17" || dir == "sp18" || dir == "fa18" || dir == "gluex1") && ext == "")	ext = dir;
+
 	Split(inf_names[0], "dat", dir, n_threads, ext, cuts);
 	Split(inf_names[1], "acc", dir, n_threads, ext, cuts);
 	Split(inf_names[2], "gen", dir, n_threads, ext, gen_cuts);
 }
 
-void Split(std::string inf_name, std::string data, std::string run, int n_threads = 4, std::string ext = "", std::string filter = "", bool verbose = false) {
+void Split(std::string inf_name, std::string data, std::string dir, int n_threads = 4, std::string ext = "", std::string filter = "", bool verbose = false) {
 	// Parallelize with n threads
 	if(n_threads > 0.0)	ROOT::EnableImplicitMT(n_threads);
 
-	// Branches you want to use get
+	// Branches you want to save get
 	std::vector<std::string> branches = {"Weight", "pol_angle", "E_Beam", "Px_Beam", "Py_Beam", "Pz_Beam", 
 				"NumFinalState", "E_FinalState", "Px_FinalState", "Py_FinalState", "Pz_FinalState", "mandel_t"};
 
@@ -167,28 +201,18 @@ void Split(std::string inf_name, std::string data, std::string run, int n_thread
 	}
 	
 	// make new data frame with cuts
-	//auto dat000 = df.Filter( ("Weight == 1 && pol_angle == 0"+filter).c_str() );
-	//auto dat045 = df.Filter( ("Weight == 1 && pol_angle == 45"+filter).c_str() );
-	//auto dat090 = df.Filter( ("Weight == 1 && pol_angle == 90"+filter).c_str() );
-	//auto dat135 = df.Filter( ("Weight == 1 && pol_angle == 135"+filter).c_str() );
-
 	auto dat000 = df.Filter( "amptools_dat == 1 && pol_angle == 0" );
 	auto dat045 = df.Filter( "amptools_dat == 1 && pol_angle == 45" );
 	auto dat090 = df.Filter( "amptools_dat == 1 && pol_angle == 90" );
 	auto dat135 = df.Filter( "amptools_dat == 1 && pol_angle == 135" );
 
-	//auto dat000 = df.Filter( ("pol_angle == 0"+filter).c_str() );
-	//auto dat045 = df.Filter( ("pol_angle == 45"+filter).c_str() );
-	//auto dat090 = df.Filter( ("pol_angle == 90"+filter).c_str() );
-	//auto dat135 = df.Filter( ("pol_angle == 135"+filter).c_str() );
-
 	std::cout << std::endl << "Filters have been defines next we take a Snapshot" << std::endl;
 
 	// save falt tree with specific branches
-	dat000.Snapshot("kin", (run+ext+"/"+data+"000.root").c_str(), branches);
-	dat045.Snapshot("kin", (run+ext+"/"+data+"045.root").c_str(), branches);
-	dat090.Snapshot("kin", (run+ext+"/"+data+"090.root").c_str(), branches);
-	dat135.Snapshot("kin", (run+ext+"/"+data+"135.root").c_str(), branches);
+	dat000.Snapshot("kin", (dir+"/"+data+"_"+ext+"_000.root").c_str(), branches);
+	dat045.Snapshot("kin", (dir+"/"+data+"_"+ext+"_045.root").c_str(), branches);
+	dat090.Snapshot("kin", (dir+"/"+data+"_"+ext+"_090.root").c_str(), branches);
+	dat135.Snapshot("kin", (dir+"/"+data+"_"+ext+"_135.root").c_str(), branches);
 
 	std::cout << std::endl << "Snapshots have completed" << std::endl;
 	std::cout << "Number of entries saved " << *dat000.Count() + *dat045.Count() + *dat090.Count() + *dat135.Count() << std::endl;
@@ -196,20 +220,28 @@ void Split(std::string inf_name, std::string data, std::string run, int n_thread
 	if(data == "dat") {
 		std::cout << std::endl << "Proccesing background files" << std::endl;
 
-		//auto bkg000 = df.Filter("Weight != 1 && pol_angle == 0"+filter);
-		//auto bkg045 = df.Filter("Weight != 1 && pol_angle == 45"+filter);
-		//auto bkg090 = df.Filter("Weight != 1 && pol_angle == 90"+filter);
-		//auto bkg135 = df.Filter("Weight != 1 && pol_angle == 135"+filter);
 		auto bkg000 = df.Filter("pol_angle == 0 && amptools_bkg == 1");
 		auto bkg045 = df.Filter("pol_angle == 45 && amptools_bkg == 1");
 		auto bkg090 = df.Filter("pol_angle == 90 && amptools_bkg == 1");
 		auto bkg135 = df.Filter("pol_angle == 135 && amptools_bkg == 1");
 
-		bkg000.Snapshot("kin", (run+ext+"/bkg000.root").c_str(), branches);
-		bkg045.Snapshot("kin", (run+ext+"/bkg045.root").c_str(), branches);
-		bkg090.Snapshot("kin", (run+ext+"/bkg090.root").c_str(), branches);
-		bkg135.Snapshot("kin", (run+ext+"/bkg135.root").c_str(), branches);
+		bkg000.Snapshot("kin", (dir+"/bkg_"+ext+"_000.root").c_str(), branches);
+		bkg045.Snapshot("kin", (dir+"/bkg_"+ext+"_045.root").c_str(), branches);
+		bkg090.Snapshot("kin", (dir+"/bkg_"+ext+"_090.root").c_str(), branches);
+		bkg135.Snapshot("kin", (dir+"/bkg_"+ext+"_135.root").c_str(), branches);
 
 		std::cout << std::endl << "Snapshot of background files has been taken" << std::endl;
 	}
+}
+
+std::string set_cuts(std::map<std::string, std::string> cuts_list, std::pair<std::string, std::string> change_cut = {"", ""}) {
+	std::string cuts = "";
+
+	for(auto it = cuts_list.begin(); it != cuts_list.end(); ++it) {
+		if(it->first == change_cut.first)	cuts += change_cut.second + " && ";
+		else								cuts += it->second + " && ";
+	}
+	cuts.erase(cuts.size()-4, 4); // remove last " && "
+
+	return cuts;
 }
