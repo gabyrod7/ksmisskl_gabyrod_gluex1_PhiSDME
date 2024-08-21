2f5d1997f6a4b07bcc4247545597b920aa8eaf45
Gabriel A Rodriguez Linera <gabyrod7@gmail.com>
2024-08-21 08:32:37 -0400

    update setup script

--- sdme/fits/setup_new_fit_dir.sh
+++ sdme/fits/setup_new_fit_dir.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-if [ -z "$2" ]
+if [ -z "$1" ]
 then
-	echo "Missing directory name or run period. Run as:"
-	echo "./setup_new_fit_dir.sh <direcotry_name> <run_period>"
+	echo "Missing directory name. Run as:"
+	echo "./setup_new_fit_dir.sh <direcotry_name>"
 # check if directory name end with /
 elif [[ $1 == */ ]]
 then
-	echo "Directory name should not end with /"
+	echo $1=$1"/"
 else
 	mkdir $1
 	cp ../sdme_scripts/* $1
 	cd $1
 	sed -i s/sdme_dir/$1/ cmd.sh
-	sed -i s/DATA/$2/g divideData.pl
-	sed -i s/DATA/$2/g fitsdme.py
-	sed -i s/DATA/$2/g plotter.C
 fi
-
